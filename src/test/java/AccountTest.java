0439dee84ab91034cbda34831f71aa471cfff1d9
Tristan Lasalle <tristan.lasalle@lacombedulionvert.fr>
2019-09-09 14:03:40 +0200

    add triangulation test

--- src/test/java/AccountTest.java
+++ src/test/java/AccountTest.java
@@ -16,6 +16,19 @@ public class AccountTest {
         assertEquals(expectedBalance, account.checkBalance());
     }
 
+    @Test
+    public void balance_should_grows_when_deposit_twice() {
+        Account account = new Account();
+        Amount expectedBalance = amountOf(2000);
+        account.add(amountOf(1200));
+        account.add(amountOf(800));
+
+        Amount actual = account.checkBalance();
+
+        assertEquals(expectedBalance, actual);
+    }
+
+
     private Amount amountOf(int val) {
         return new Amount(new BigDecimal(val));
     }
